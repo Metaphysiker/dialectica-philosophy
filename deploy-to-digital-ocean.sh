34e0a67e824ba78a3d1d113711c808b90b78e262
Metaphysiker <s.raess@me.com>
2022-12-06 21:29:12 +0100

    precompile

--- deploy-to-digital-ocean.sh
+++ deploy-to-digital-ocean.sh
@@ -1,3 +1,23 @@
-bundle config set --local deployment 'true'
-bundle config set --local without 'development test'
-bundle install
+#!/bin/bash
+rails assets:clobber
+RAILS_ENV=production rails assets:precompile
+git add .
+git commit -m "precompile"
+git push origin main
+
+
+ssh sandro@159.65.120.231 << EOF
+  cd dialectica-philosophy
+  git stash
+  git pull origin main
+  bundle config set --local without 'development test'
+  bundle install
+  RAILS_ENV=production rails db:migrate
+EOF
+
+ssh root@159.65.120.231 << EOF
+  cd /home/sandro/dialectica-philosophy/
+  sudo service nginx restart
+EOF
+
+yarn install --check-files
